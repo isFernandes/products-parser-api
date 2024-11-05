c3cb7b8d373eeba4c3658013c7c79b32b2a606e3
isFernandes <iago.stosfernandes@gmail.com>
2024-11-04 22:45:31 -0300

    reorder functions

--- src/services/import.service.ts
+++ src/services/import.service.ts
@@ -15,6 +15,18 @@ export class ImportService {
   @Inject("productRepository")
   private productRepository!: ProductRepository;
 
+  async importData() {
+    try {
+      const names = await this.getFilenames();
+
+      for await (const name of names) {
+        if (name.length === 19) await this.processData(name);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   private async getFilenames() {
     const filenames: string[] = [];
 
@@ -58,6 +70,42 @@ export class ImportService {
     return data.split("\n").filter((el: string) => el.length > 1);
   }
 
+  private async processData(filename: string) {
+    const returnedData = await this.callApi(
+      `https://challenges.coode.sh/food/data/json/${filename}`
+    );
+
+    let fileOffset: number = await this.getOffset(filename);
+
+    const dataToStream = Readable.fromWeb(returnedData);
+
+    const products: IProduct[] = [];
+
+    dataToStream
+      .pipe(createUnzip({ finishFlush: constants.Z_SYNC_FLUSH }))
+      .pipe(this.limitOffset(fileOffset))
+      .pipe(this.extractProductLine())
+      .pipe(this.parseProductObject())
+      .on("data", async ({ payload, newOffset }) => {
+        products.push(payload);
+        fileOffset = newOffset > fileOffset ? newOffset : fileOffset;
+      })
+      .on("finish", async () => {
+        if (products.length > 0) {
+          await this.importRepository.save({
+            quantity: products.length,
+            date: new Date(),
+            source: filename,
+            offset: fileOffset,
+          });
+
+          await this.productRepository.saveMany(products);
+        }
+      });
+
+    return dataToStream;
+  }
+
   private async getOffset(filename: string) {
     try {
       const offset = await this.importRepository.getLastOffset(filename);
@@ -67,55 +115,26 @@ export class ImportService {
     }
   }
 
-  async importData() {
-    try {
-      const names = await this.getFilenames();
-
-      for await (const name of names) {
-        if (name.length === 19) await this.processData(name);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  private parseProductObject() {
+  private limitOffset(fileOffset: number) {
     return new Transform({
-      objectMode: true,
       transform(chunk, _, callback) {
-        const productData = JSON.parse(chunk.toString());
-        const sanitizedProduct = {
-          code: productData.code.replace(/\D/g, "").toString(),
-          url: productData.url,
-          creator: productData.creator,
-          created_t: productData.created_t,
-          last_modified_t: productData.last_modified_t,
-          product_name: productData.product_name,
-          quantity: productData.quantity,
-          brands: productData.brands,
-          categories: productData.categories,
-          labels: productData.labels,
-          cities: productData.cities,
-          purchase_places: productData.purchase_places,
-          stores: productData.stores,
-          ingredients_text: productData.ingredients_text,
-          traces: productData.traces,
-          serving_size: productData.serving_size,
-          serving_quantity: productData.serving_quantity,
-          nutriscore_score: productData.nutriscore_score,
-          nutriscore_grade: productData.nutriscore_grade,
-          main_category: productData.main_category,
-          image_url: productData.image_url,
-          imported_t: new Date() as Date,
-          status: IProductStatus.DRAFT,
-        };
+        const buffer = Buffer.from(chunk);
 
-        this.emit("data", {
-          payload: sanitizedProduct,
-          productCount: productData.count,
-          newOffset: productData.newOffset,
-        });
+        if (fileOffset > 0) {
+          if (buffer.length <= fileOffset) {
+            fileOffset -= buffer.length;
+            return callback();
+          } else {
+            const start = fileOffset;
+            const end = buffer.length;
 
+            fileOffset = 0;
+
+            this.push(buffer.subarray(start, end));
+          }
+        } else {
+          this.push(buffer);
+        }
         callback();
       },
     });
@@ -166,71 +185,53 @@ export class ImportService {
     });
   }
 
+  private parseProductObject() {
+    return new Transform({
+      objectMode: true,
+      transform(chunk, _, callback) {
+        const productData = JSON.parse(chunk.toString());
+        const sanitizedProduct = {
+          code: productData.code.replace(/\D/g, "").toString(),
+          url: productData.url,
+          creator: productData.creator,
+          created_t: productData.created_t,
+          last_modified_t: productData.last_modified_t,
+          product_name: productData.product_name,
+          quantity: productData.quantity,
+          brands: productData.brands,
+          categories: productData.categories,
+          labels: productData.labels,
+          cities: productData.cities,
+          purchase_places: productData.purchase_places,
+          stores: productData.stores,
+          ingredients_text: productData.ingredients_text,
+          traces: productData.traces,
+          serving_size: productData.serving_size,
+          serving_quantity: productData.serving_quantity,
+          nutriscore_score: productData.nutriscore_score,
+          nutriscore_grade: productData.nutriscore_grade,
+          main_category: productData.main_category,
+          image_url: productData.image_url,
+          imported_t: new Date() as Date,
+          status: IProductStatus.DRAFT,
+        };
+
+        this.emit("data", {
+          payload: sanitizedProduct,
+          productCount: productData.count,
+          newOffset: productData.newOffset,
+        });
+
+        callback();
+      },
+    });
+  }
+
   async getLastImport() {
     try {
       return await this.importRepository.getLastUpdate();
     } catch (error) {
       throw new HttpError(500, error as string);
     }
-  }
-
-  private limitOffset(fileOffset: number) {
-    return new Transform({
-      transform(chunk, _, callback) {
-        const buffer = Buffer.from(chunk);
-
-        if (fileOffset > 0) {
-          if (buffer.length <= fileOffset) {
-            fileOffset -= buffer.length;
-            return callback();
-          } else {
-            const start = fileOffset;
-            const end = buffer.length;
-
-            fileOffset = 0;
-
-            this.push(buffer.subarray(start, end));
-          }
-        } else {
-          this.push(buffer);
-        }
-        callback();
-      },
-    });
-  }
-
-  private async processData(filename: string) {
-    const returnedData = await this.callApi(
-      `https://challenges.coode.sh/food/data/json/${filename}`
-    );
-
-    let fileOffset: number = await this.getOffset(filename);
-
-    const dataToStream = Readable.fromWeb(returnedData);
-
-    const products: IProduct[] = [];
-
-    dataToStream
-      .pipe(createUnzip({ finishFlush: constants.Z_SYNC_FLUSH }))
-      .pipe(this.limitOffset(fileOffset))
-      .pipe(this.extractProductLine())
-      .pipe(this.parseProductObject())
-      .on("data", async ({ payload, newOffset }) => {
-        products.push(payload);
-        fileOffset = newOffset > fileOffset ? newOffset : fileOffset;
-      })
-      .on("finish", async () => {
-        await this.importRepository.save({
-          quantity: products.length,
-          date: new Date(),
-          source: filename,
-          offset: fileOffset,
-        });
-
-        if (products.length > 0)
-          await this.productRepository.saveMany(products);
-      });
-
-    return dataToStream;
   }
 }
